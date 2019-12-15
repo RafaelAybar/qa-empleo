4af454fec86687dd98ebb70d57337c39c254d249
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2019-12-15 17:09:07 +0100

    Finalizada la parte de los puntos

--- qa-empleo.py
+++ qa-empleo.py
@@ -1,22 +1,34 @@
 import sys
 
-paramsList = [sys.argv[1].strip(), sys.argv[2].strip(), sys.argv[3].strip(), sys.argv[4].strip(), sys.argv[5].strip(),
-              sys.argv[6].strip(), sys.argv[7].strip(), sys.argv[8].strip()]
+# https://www.digitalocean.com/community/tutorials/how-to-convert-data-types-in-python-3
 
-
-def menuinicial():
-    print("Bienvenidos a qa-empleo, introduce el número para seleccionar una opción")
+paramsList = [sys.argv[1].strip(), int(sys.argv[2].strip()), int(sys.argv[3].strip()), int(sys.argv[4].strip()), int(sys.argv[5].strip()),
+              int(sys.argv[6].strip()), int(sys.argv[7].strip()), int(sys.argv[8].strip())]
 
 
 def calculamedia():
     total = 0
     media = 0
     # http://lineadecodigo.com/python/iterar-una-lista-en-python-con-indices/
-    for a in range(1, len(paramsList)):
-        total += paramsList[a]
+    for a in range(2, len(paramsList)):
+        total = total + paramsList[a]
     if total == 0:
         print("No hay datos suficientes")
     else:
         media = total / 7
-    print("La puntuación media obtenida es: " + media)
+    print(f"La puntuación media obtenida es: {media}")
     return media
+
+
+nota = calculamedia()
+estado = ""
+if nota <= 6:
+    estado = "mal"
+
+elif nota > 4.2 < 10:
+    estado = "bien"
+else:
+    print("Debes introducir valores coherentes")
+    exit(1)
+
+print(f"La oferta está {estado}")
