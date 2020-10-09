1e6fce442fb4bdb26d218df38d1ddcb278ee1213
Victor Yelicich <victor.yelicich@usach.cl>
2020-10-09 21:22:54 -0200

    Refactorización y parseado de entradas

---
+++ qa_empleo.py
@@ -0,0 +1,82 @@
+"""Package Qa Empleo
+
+Returns:
+    estado: ['bien', 'mal']
+"""
+__version__ = "0.1"
+
+import sys
+import argparse
+
+
+class Empresa:
+
+    def __init__(self, params: dict) -> None:
+        self.params = params
+
+    def _calcula_media_notas(self) -> float:
+        """
+        _calcula_media_notas() toma las claves de cada nota, accede a los valores
+        llamando al atributo (dict) 'params' (ignorando nombre_empresa) y calcula
+        la media aritmética de las notas redondeada a 1 decimal.
+
+        """
+        suma = 0
+        media = 0
+        claves_notas = list(self.params.keys())
+        claves_notas.remove('nombre_empresa')
+        for clave_nota in claves_notas:
+            suma += self.params[clave_nota]
+        media = round(suma / 7, 1)
+        return media
+
+    def obtener_estado(self) -> None:
+        """
+        obtener_estado() toma la nota media e imprime por pantalla la clasificación 
+        de dicha nota.
+
+        """
+        estado = ''
+        nota_media = self._calcula_media_notas()
+        if -10 <= nota_media <= 6:
+            estado = 'mal'
+        elif 6 < nota_media <= 10:
+            estado = 'bien'
+        else:
+            estado = 'Debes introducir valores coherentes'
+            sys.exit(estado)
+        print(f'La puntuación media obtenida es: {nota_media}')
+        print(f'La oferta está {estado}')
+
+
+class AnalizadorSintactico(argparse.ArgumentParser):
+
+    @classmethod
+    def parsear_entrada(cls) -> dict:
+        """
+        parsear_entrada() recibe los parámetros de la línea de comandos, realiza el análisis
+        sintáctico de cada entrada (en función del tipo y alternativas de valores) y retorna 
+        un diccionario con sus valores.
+
+        """
+        parser = cls(prog='qa-empleo', add_help=False)
+        parser.add_argument('nombre_empresa', type=str)
+        parser.add_argument('nota_tipo_contrato', type=int, choices=[-10, 1, 5, 10])
+        parser.add_argument('nota_ubicacion_cliente', type=int, choices=[0, 5, 10])
+        parser.add_argument('nota_requerimientos_cliente', type=int, choices=[0, 5, 10])
+        parser.add_argument('nota_vacaciones', type=int, choices=[0, 5, 10])
+        parser.add_argument('nota_salario', type=int, choices=[-10, 0, 5, 10])
+        parser.add_argument('nota_horario', type=int, choices=[0, 5, 10])
+        parser.add_argument('nota_teletrabajo', type=int, choices=[0, 5, 10])
+        entrada = vars(parser.parse_args())
+        return entrada
+
+
+def ejecutar():
+    params = AnalizadorSintactico.parsear_entrada()
+    MiEmpresa = Empresa(params=params)
+    MiEmpresa.obtener_estado()
+
+
+if __name__ == '__main__':
+    ejecutar()
